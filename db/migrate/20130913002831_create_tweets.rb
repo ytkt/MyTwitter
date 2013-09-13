b47aa16b2a4e2a295f2390baef83f78d17f6464f
Yuta Kato <yyttkktt@gmail.com>
2013-09-13 09:32:57 +0900

    Tweetモデルに制約を追加

--- db/migrate/20130913002831_create_tweets.rb
+++ db/migrate/20130913002831_create_tweets.rb
@@ -1,7 +1,7 @@
 class CreateTweets < ActiveRecord::Migration
   def change
     create_table :tweets do |t|
-      t.string :body
+      t.string :body, :null => false, :limit => 140
 
       t.timestamps
     end
