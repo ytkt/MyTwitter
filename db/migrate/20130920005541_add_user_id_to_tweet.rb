fd4e0b499d044ce14533e7c47dece1748b36fcfa
Yuta Kato <yyttkktt@gmail.com>
2013-09-20 09:58:58 +0900

    説明を追記

--- db/migrate/20130920005541_add_user_id_to_tweet.rb
+++ db/migrate/20130920005541_add_user_id_to_tweet.rb
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
 class AddUserIdToTweet < ActiveRecord::Migration
   def change
-    add_column :tweets, :user_id, :integer
+    add_column(:tweets, :user_id, :integer)
+    # add_column(テーブル名 カラム名, タイプ [, オプション])
   end
 end
