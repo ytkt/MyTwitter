c8137b3e6de196b414713c477884da59bc46fbe9
Yuta Kato <yyttkktt@gmail.com>
2013-09-20 09:59:38 +0900

    マイグレーションスクリプトを実行
    $ rake db:migrate
    ==  AddUserIdToTweet: migrating ===============================================
    -- add_column(:tweets, :user_id, :integer)
       -> 0.0073s
    ==  AddUserIdToTweet: migrated (0.0074s) ======================================

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130920003112) do
+ActiveRecord::Schema.define(version: 20130920005541) do
 
   create_table "tweets", force: true do |t|
     t.string   "body",       limit: 140, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
