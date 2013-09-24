bd070f5579be49b6d7f0567e08770a5ad15ba583
Yuta Kato <yyttkktt@gmail.com>
2013-09-24 10:12:37 +0900

    twitterコールバックのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Twitter::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :tweets
   root 'tweets#index'
 
